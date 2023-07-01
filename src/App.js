ca41100af127144ce61bdcdc8e1feaaae8c74969
aps <aravindpsabu@gmail.com>
2023-07-01 15:10:20 +0530

    Added calculateWinner to Board

--- src/App.js
+++ src/App.js
@@ -8,17 +8,38 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() {
 
+  const [xIsNext, setXIsNext] = useState(true)
   const [squares, setSquares] = useState(Array(9).fill(null))
 
+  const winner = calculateWinner(squares)
+  let status
+  if (winner) {
+    status = "Winner: " + winner
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O")
+  }
+
   function handleClick(i) {
+
+    if (squares[i] || calculateWinner(squares)) {
+      return
+    }
+
+
     let newSquareArray = squares.slice()
-    newSquareArray[i] = "X"
+    if (xIsNext) {
+      newSquareArray[i] = "X"
+    } else {
+      newSquareArray[i] = "O"
+    }
     setSquares(newSquareArray)
+    setXIsNext(!xIsNext)
 
   }
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -36,4 +57,26 @@ export default function Board() {
       </div>
     </>
   )
+
+  function calculateWinner(squares) {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8]
+    ]
+
+    for (let winningCombination of winningCombinations) {
+      const [a, b, c] = winningCombination
+      if (squares[a] && squares[a] == squares[b] && squares[a] == squares[c]) {
+        return squares[a]
+      }
+
+      return null
+    }
+  }
 }
