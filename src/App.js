74e5ed4344750ed7f77ba4e32685cd8f30d13d1e
aps <aravindpsabu@gmail.com>
2023-07-06 23:34:36 +0530

    Updated calculateWinner in Board

--- src/App.js
+++ src/App.js
@@ -62,9 +62,9 @@ export default function Board() {
     const winningCombinations = [
       [0, 1, 2],
       [3, 4, 5],
-      [6, 7, 8],
-      [0, 4, 8],
-      [2, 4, 6],
+      [6, 7, 8],           
+      [0, 4, 8],           
+      [2, 4, 6],           
       [0, 3, 6],
       [1, 4, 7],
       [2, 5, 8]
@@ -76,7 +76,8 @@ export default function Board() {
         return squares[a]
       }
 
-      return null
     }
+
+   return null  
   }
 }
